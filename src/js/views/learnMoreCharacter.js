496b3485a463dafe99f151f1a11c131a97fd8d13
Martín Suárez <ms.prgramador@gmail.com>
2021-09-28 17:44:45 +0000

    added learn more character

--- src/js/views/learnMoreCharacter.js
+++ src/js/views/learnMoreCharacter.js
@@ -11,35 +11,44 @@ const LearnMoreCharacter = (props) => {
                 </div>
             </div>
             <hr />
-            <table border="1">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Birth Year</th>
-                        <th>Gender</th>
-                        <th>Height</th>
-                        <th>Skin Color</th>
-                        <th>Eye Color</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tbody>
+            <div className="row">
+                <div className="col-12">
+                    <table border="1">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Birth Year</th>
+                                <th>Gender</th>
+                                <th>Height</th>
+                                <th>Skin Color</th>
+                                <th>Eye Color</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{props.name}</td>
+                                <td>{props.birth_year}</td>
+                                <td>{props.gender}</td>
+                                <td>{props.height}</td>
+                                <td>{props.skin_color}</td>
+                                <td>{props.eye_color}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
 
-            </table>
         </div>
     );
 }
 
 LearnMoreCharacter.propTypes = {
-    
+    name: PropTypes.string,
+    birth_year: PropTypes.string.apply,
+    gender: PropTypes.string,
+    height: PropTypes.string,
+    skin_color: PropTypes.string,
+    eye_color: PropTypes.string
 }
 
 export default LearnMoreCharacter;
