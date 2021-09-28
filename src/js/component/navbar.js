8693495f87a6807d2fdec50044b6a67a1cfc344d
Martín Suárez <ms.prgramador@gmail.com>
2021-09-28 03:15:53 +0000

    va en camino

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,13 +4,40 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+			<div className="container">
+				{/* <Link to="/"> */}
+				<span className="navbar-brand mb-0 h1">
+					<img src="https://www.freepnglogos.com/uploads/star-wars-logo-31.png" style={{ width: "70px" }} />
+				</span>
+				{/* </Link> */}
+				{/* <div> */}
+				{/* <Link to="/demo"> */}
+				<div className="dropdown show">
+					<a
+						className="btn btn-secondary dropdown-toggle"
+						href="#"
+						role="button"
+						id="dropdownMenuLink"
+						data-toggle="dropdown"
+						aria-haspopup="true"
+						aria-expanded="false">
+						Dropdown link
+					</a>
+
+					<div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+						<a className="dropdown-item" href="#">
+							Action
+						</a>
+						<a className="dropdown-item" href="#">
+							Another action
+						</a>
+						<a className="dropdown-item" href="#">
+							Something else here
+						</a>
+					</div>
+				</div>
+				{/* </Link> */}
+				{/* </div> */}
 			</div>
 		</nav>
 	);
