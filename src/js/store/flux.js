cba8d09fc9ebcd31759960ebab44b73b8e4a01da
Martín Suárez <ms.prgramador@gmail.com>
2021-09-28 00:30:04 +0000

    how it go

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,5 +1,6 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	const URL = "https://www.swapi.tech/api/people/";
+	const URL_PERSONAJES = "https://www.swapi.tech/api/people?page=1&limit=100";
+	const URL_PLANETAS = "https://www.swapi.tech/api/planets?page=1&limit=100";
 	return {
 		store: {
 			demo: [
@@ -14,26 +15,39 @@ const getState = ({ getStore, getActions, setStore }) => {
 					initial: "white"
 				}
 			],
-			listaPersonajes: []
+			listaPersonajes: [],
+			listaPlanetas: []
 		},
 		actions: {
-			addPersonajes: personaje => {
-				const store = getStore();
-				setStore({ listaPersonajes: [...store.listaPersonajes, personaje] });
-			},
+			// addPersonajes: personaje => {
+			// 	const store = getStore();
+			// 	setStore({ listaPersonajes: [...store.listaPersonajes, personaje] });
+			// },
+			// addPlanetas: planeta => {
+			// 	const store = getStore();
+			// 	setStore({ listaPlanetas: [...store.listaPlanetas, planeta] });
+			// },
 
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
 			loadSomeData: () => {
-				fetch(URL)
+				fetch(URL_PERSONAJES)
 					.then(res => res.json())
 					.then(response => {
-						store.actions.addPersonajes(response.results);
-						console.log(response.results);
+						// store.actions.addPersonajes(response.results);
+						console.log(response);
+						setStore({ listaPersonajes: response.results });
 					})
 					.catch(err => err);
+
+				fetch(URL_PLANETAS)
+					.then(res => res.json())
+					.then(data => {
+						setStore({ listaPlanetas: data.results });
+					})
+					.catch();
 			},
 			changeColor: (index, color) => {
 				//get the store
