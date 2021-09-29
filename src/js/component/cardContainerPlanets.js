9069c3571fb41a0639c2eb13794a3bbe4cf48e9b
Martín Suárez <ms.prgramador@gmail.com>
2021-09-29 01:06:09 +0000

    card rendered

--- src/js/component/cardContainerPlanets.js
+++ src/js/component/cardContainerPlanets.js
@@ -6,9 +6,12 @@ const CardContainerPlanets = props => {
 	return (
 		<>
 			<h1 className="text-danger">Planets</h1>
-			<div className="container d-flex" style={{ overflow: "auto" }}>
+			<div
+				className="scrolling-wrapper row flex-row flex-nowrap row-cols-1 row-cols-md-3 
+"
+				style={{ overflowX: "scroll", overflowY: "hidden", whiteSpace: "nowrap" }}>
 				{store.listaPlanetas.map((item, index) => {
-					<Card key={item.uid} name={item.name} url={item.url} />;
+					return <Card key={item.uid} name={item.name} url={item.url} />;
 				})}
 			</div>
 		</>
