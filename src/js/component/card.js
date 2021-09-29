c186b143b2d2318475b4d8ce2ff5322a619e89f7
Martín Suárez <ms.prgramador@gmail.com>
2021-09-29 01:19:44 +0000

    card flexed

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -3,15 +3,17 @@ import PropTypes from "prop-types";
 
 const Card = props => {
 	return (
-		<div className="card" style={{ width: "30%", display: "inline-block", margin: "10px" }}>
-			<img className="card-img-top" src="..." alt="Card image cap" />
+		<div className="card" style={{ width: "auto", display: "inline-block", margin: "10px" }}>
+			<img className="card-img-top" src="https://via.placeholder.com/400x200" alt="Card image cap" />
 			<div className="card-body">
 				<h5 className="card-title">{props.name}</h5>
 				<p className="card-text">{props.url}</p>
-				<a href="#" className="btn btn-primary">
-					Learn More!
-				</a>
-				<i className="fa-light fa-heart rounded" />
+				<div className="d-flex justify-content-between">
+					<a href="#" className="btn btn-primary">
+						Learn More!
+					</a>
+					<i className="fa-light fa-heart rounded" />
+				</div>
 			</div>
 		</div>
 	);
